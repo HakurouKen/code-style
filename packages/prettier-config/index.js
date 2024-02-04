f1abb2e3a496f49e6f16578efed9f85509912abe
leroyxyli <leroyxyli@tencent.com>
2024-02-04 14:15:00 +0800

    feat: prettier config

---
+++ packages/prettier-config/index.js
@@ -0,0 +1,17 @@
+// @ts-check
+
+/** @type {import("prettier").Config} */
+const config = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'strict',
+  arrowParens: 'always'
+};
+
+export default config;
