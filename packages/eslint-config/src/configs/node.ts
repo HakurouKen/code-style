c2b4b466acf160ea4071db71888c8e757633dddf
leroyxyli <leroyxyli@tencent.com>
2024-02-23 16:22:28 +0800

    feat: turn off import/require rules of plugin-n

--- packages/eslint-config/src/configs/node.ts
+++ packages/eslint-config/src/configs/node.ts
@@ -1,5 +1,19 @@
 import pluginNode from 'eslint-plugin-n';
 
 export function node() {
-  return [pluginNode.configs['flat/recommended']];
+  return [
+    pluginNode.configs['flat/recommended'],
+    {
+      // Disable all require/import rules, cause these features should be handle by build tools.
+      rules: {
+        'n/no-extraneous-import': 'off',
+        'n/no-extraneous-require': 'off',
+        'n/no-missing-import': 'off',
+        'n/no-missing-require': 'off',
+        'n/no-unpublished-bin': 'off',
+        'n/no-unpublished-import': 'off',
+        'n/no-unpublished-require': 'off'
+      }
+    }
+  ];
 }
