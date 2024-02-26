70484703b531213be2235d5cf6a5df636f09c19c
leroyxyli <leroyxyli@tencent.com>
2024-02-26 11:53:54 +0800

    feat: disable jsdoc-types in typescript files

--- packages/eslint-config/src/configs/jsdoc.ts
+++ packages/eslint-config/src/configs/jsdoc.ts
@@ -1,10 +1,22 @@
 import pluginJsdoc from 'eslint-plugin-jsdoc';
+import { GLOB_TS, GLOB_TSX } from '../matchers';
 
 export async function jsdoc() {
   return [
     pluginJsdoc.configs['flat/recommended'],
     {
       rules: { 'jsdoc/require-jsdoc': 'off' }
+    },
+    {
+      files: [GLOB_TS, GLOB_TSX],
+      rules: {
+        'jsdoc/check-tag-names': ['warn', { typed: true }],
+        'jsdoc/no-types': 'warn',
+        'jsdoc/no-undefined-types': 'off',
+        'jsdoc/require-param-type': 'off',
+        'jsdoc/require-property-type': 'off',
+        'jsdoc/require-returns-type': 'off'
+      }
     }
   ];
 }
