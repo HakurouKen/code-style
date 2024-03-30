a440f0ff3d88c343a5fb0aed55ff55bf410e8935
leroyxyli <leroyxyli@tencent.com>
2024-03-30 19:33:57 +0800

    refactor: compatExtends

--- packages/eslint-config/src/configs/typescript.ts
+++ packages/eslint-config/src/configs/typescript.ts
@@ -1,19 +1,10 @@
-import path from 'node:path';
-import url from 'node:url';
-
-import { FlatCompat } from '@eslint/eslintrc';
+import { compatExtends } from '../utils';
 import type { OptionsTypescript } from '../types';
 
 const GLOB_SRC = '**/*.?([cm])[jt]s?(x)';
-const compat = new FlatCompat({
-  resolvePluginsRelativeTo:
-    typeof __dirname === 'undefined'
-      ? path.dirname(url.fileURLToPath(import.meta.url))
-      : __dirname
-});
 
 export async function typescript(options: OptionsTypescript) {
-  const configs = compat.extends('plugin:@typescript-eslint/recommended');
+  const configs = compatExtends('plugin:@typescript-eslint/recommended');
   const files = options.files ?? [GLOB_SRC];
 
   configs.push({
--- packages/eslint-config/src/utils.ts
+++ packages/eslint-config/src/utils.ts
@@ -1,3 +1,7 @@
+import path from 'node:path';
+import url from 'node:url';
+import { FlatCompat } from '@eslint/eslintrc';
+
 export type Awaitable<T> = T | Promise<T>;
 
 export async function interopDefault<T>(
@@ -5,4 +9,15 @@ export async function interopDefault<T>(
 ): Promise<T extends { default: infer U } ? U : T> {
   const resolved: any = await m;
   return resolved.default ?? resolved;
+}
+
+export function compatExtends(name: string) {
+  const compat = new FlatCompat({
+    resolvePluginsRelativeTo:
+      typeof __dirname === 'undefined'
+        ? path.dirname(url.fileURLToPath(import.meta.url))
+        : __dirname
+  });
+
+  return compat.extends(name);
 }
